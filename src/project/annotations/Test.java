0b6c683e5e24a98dea77fad7c113ebcf60791274
Sean-1274 <wilks1@newpaltz.edu>
2025-09-03 21:53:50 -0400

    Update Test.java

--- src/project/annotations/Test.java
+++ src/project/annotations/Test.java
@@ -2,10 +2,10 @@ package project.annotations;
 
 public class Test {
 
-public static void main(String[] args) {
-// TODO Auto-generated method stub
-System.out.println("Hello")
-		
-}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
 
 }
