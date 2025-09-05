a6a6ce611ecc914476dadbb0ffdb1f7926a056be
Sean-1274 <wilks1@newpaltz.edu>
2025-09-05 13:53:01 -0400

    Test (#8)
    * Update build.gradle
    
    * Bad Code for example
    
    code does not have ending semicolon and no indentation
    
    * Update Test.java
    
    * Update Test.java
    
    * Update Test.java

--- src/project/annotations/Test.java
+++ src/project/annotations/Test.java
@@ -3,8 +3,9 @@ package project.annotations;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		int x = 10;
+		System.out.println("This will compile");
+
 
 	}
 
