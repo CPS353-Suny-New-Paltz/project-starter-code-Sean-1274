d99ec4a8f4a70958751d5423831d69642b83d1a6
Sean-1274 <wilks1@newpaltz.edu>
2025-09-05 13:38:10 -0400

    Update Test.java

--- src/project/annotations/Test.java
+++ src/project/annotations/Test.java
@@ -3,8 +3,8 @@ package project.annotations;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		 int x = 10
+			        System.out.println("This will not compile")
 
 	}
 
