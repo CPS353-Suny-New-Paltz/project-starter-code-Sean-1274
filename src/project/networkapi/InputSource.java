fa965d455c5bea200fc501805babeb04aa6c76c4
Sean-1274 <wilks1@newpaltz.edu>
2025-09-12 19:27:40 -0400

    Network api (#17)
    * Creating the NetworkAPI
    
    Initializing and adding the files needed for the networkAPI
    
    * Fixing checkstyle issues
    
    Checkstyle issues changed: package name and method declaration in DelimiterPair class
    
    * Update JobResponse.java
    
    * Fixing file Structure
    
    Fixed the file structure so that now a job request is accompanied by a job response rather than having the implementation of the response in the API prototype

---
+++ src/project/networkapi/InputSource.java
@@ -0,0 +1,9 @@
+package project.networkapi;
+
+/**
+ * Represents a generalized source of input data.
+ */
+public interface InputSource {
+	String getLocation(); // Return location as a string
+}
+
---
+++ src/project/networkapi/OutputSource.java
@@ -0,0 +1,9 @@
+package project.networkapi;
+
+/**
+ * Represents a generalized destination for output data.
+ */
+public interface OutputSource {
+	String getLocation(); // Return location as a string
+}
+
