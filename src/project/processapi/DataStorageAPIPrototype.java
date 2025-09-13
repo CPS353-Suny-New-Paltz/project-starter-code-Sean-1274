57d30dee65ad04b5c5d924c75e582deff1190fbe
Sean-1274 <wilks1@newpaltz.edu>
2025-09-13 09:07:05 -0400

    Creating the processAPI (#18)
    * Creating the processAPI
    
    adding all classes needed for the skeleton of the processapi including reused classes from networkapi to avoid cross package dependencies
    
    * Fixing CheckStyle
    
    Fixing Checkstyle for processapi after failure when trying to push

---
+++ src/project/processapi/DataStorageAPIPrototype.java
@@ -0,0 +1,38 @@
+package project.processapi;
+
+import project.annotations.ProcessAPIPrototype;
+import java.util.List;
+import java.util.ArrayList;
+
+public class DataStorageAPIPrototype implements DataStorageAPI {
+
+	@Override
+	@ProcessAPIPrototype
+	public DataReadResponse readData(DataReadRequest readRequest) {
+		// Mock data - in real implementation, this would read from actual source
+		List<Integer> mockData = List.of(5, 10, 15, 20);
+
+		return new DataReadResponse(
+				"Prototype: Successfully read mock data from " + readRequest.getInputSource().getLocation(),
+				readRequest.getInputSource().getLocation(),
+				mockData
+				);
+	}
+
+	@Override
+	@ProcessAPIPrototype
+	public DataWriteResponse writeData(DataWriteRequest writeRequest) {
+		// Simulate writing
+		String formattedOutput = String.join(
+				writeRequest.getDelimiters().getPairDelimiter(), 
+				writeRequest.getResults()
+				);
+
+		return new DataWriteResponse(
+				"Prototype: Successfully wrote mock data to " + writeRequest.getOutputSource().getLocation(),
+				writeRequest.getOutputSource().getLocation(),
+				formattedOutput,
+				writeRequest.getResults().size()
+				);
+	}
+}
