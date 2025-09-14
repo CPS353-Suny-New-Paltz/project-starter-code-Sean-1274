05226dfaab22754704f9245ae8545489971deba1
Sean-1274 <wilks1@newpaltz.edu>
2025-09-14 09:29:24 -0400

    Deleting files
    files from previous implementation merged with the new network api so i am deleting them so i can start over

--- src/project/processapi/OutputSource.java
+++
@@ -1,9 +0,0 @@
-package project.processapi;
-
-/**
- * Represents a generalized destination for output data for the data storage system.
- * This is a separate copy from the networkapi package to avoid cross-package dependencies.
- */
-public interface OutputSource {
-	String getLocation();
-}
