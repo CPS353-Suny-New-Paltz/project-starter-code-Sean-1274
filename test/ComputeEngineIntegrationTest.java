7a968568b6dc01b45ef79be666ae2e53d719c8cb
Sean-1274 <wilks1@newpaltz.edu>
2025-10-21 10:29:31 -0400

    Update ComputeEngineIntegrationTest.java

--- test/ComputeEngineIntegrationTest.java
+++ test/ComputeEngineIntegrationTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 import project.conceptualapi.ComputeEngineAPI;
 import project.conceptualapi.EmptyComputeEngineAPI;
