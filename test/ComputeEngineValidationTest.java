fe4777468a9278f26d239dbe0cc248856a9ac27f
Sean-1274 <wilks1@newpaltz.edu>
2025-11-16 16:07:41 -0500

    fixing imports

--- test/ComputeEngineValidationTest.java
+++ test/ComputeEngineValidationTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import project.conceptualapi.EmptyComputeEngineAPI;
 import project.conceptualapi.ComputationRequest;
--- test/UserComputeValidationTest.java
+++ test/UserComputeValidationTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 
 import project.networkapi.EmptyUserComputeAPI;
 import project.networkapi.InputRequest;
