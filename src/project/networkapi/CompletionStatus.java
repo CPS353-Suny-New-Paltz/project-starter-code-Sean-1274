8ff940bf20fcf4a73c759893149606255ef40973
Sean-1274 <wilks1@newpaltz.edu>
2025-12-10 11:03:45 -0500

    Adding the Async functionality to allow user to track job status

--- src/project/networkapi/CompletionStatus.java
+++ src/project/networkapi/CompletionStatus.java
@@ -5,5 +5,8 @@ public enum CompletionStatus {
     JOB_RUNNING,     // Job is still processing
     JOB_COMPLETED,   // Job finished successfully
     JOB_FAILED,      // Job failed with errors
-    JOB_NOT_FOUND    // Job ID does not exist
+    JOB_NOT_FOUND,    // Job ID does not exist
+    JOB_CANCELLED, 
+    JOB_SUBMITTED,
+    
 }
